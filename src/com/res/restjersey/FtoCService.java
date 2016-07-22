204e8217bdd8f60d7ec78a2f2283f5b62b5bd22a
scastell <sergiocastellote85@gmail.com>
2016-07-22 12:36:41 +0200

    crear clase usuarios y que el get devuelva usuarios

--- src/com/res/restjersey/FtoCService.java
+++ src/com/res/restjersey/FtoCService.java
@@ -15,23 +15,44 @@ public class FtoCService {
 
     @GET
     @Produces({ MediaType.APPLICATION_JSON })
-    public List<Data> getData() {
-        List<Data> result = new LinkedList<>();
-        result.add(new Data(1, "one"));
-        result.add(new Data(2, "two"));
+    public List<Usuarios> getData() {
+        List<Usuarios> result = new LinkedList<>();
+        result.add(new Usuarios("Sergio", "Castellote"));
+        result.add(new Usuarios("Pere", "Martinez"));
         return result;
     }
 
     @GET
     @Produces({ MediaType.APPLICATION_JSON })
     @Path("{id}")
-    public Data getData(@PathParam("id") String id) {
+    public String getData(@PathParam("id") String id) {
         if ("1".equals(id)) {
-            return new Data(1, "one");
+            return ("one");
         }
         if ("2".equals(id)) {
-            return new Data(2, "two");
+            return ("two");
         }
         throw new WebApplicationException(404);
     }
+    // @GET
+    // @Produces({ MediaType.APPLICATION_JSON })
+    // public List<Data> getData() {
+    // List<Data> result = new LinkedList<>();
+    // result.add(new Data(1, "one"));
+    // result.add(new Data(2, "two"));
+    // return result;
+    // }
+
+    // @GET
+    // @Produces({ MediaType.APPLICATION_JSON })
+    // @Path("{id}")
+    // public Data getData(@PathParam("id") String id) {
+    // if ("1".equals(id)) {
+    // return new Data(1, "one");
+    // }
+    // if ("2".equals(id)) {
+    // return new Data(2, "two");
+    // }
+    // throw new WebApplicationException(404);
+    // }
 }
---
+++ src/com/res/restjersey/Usuarios.java
@@ -0,0 +1,31 @@
+package com.res.restjersey;
+
+public class Usuarios {
+
+    private String name;
+    private String surname;
+
+    public Usuarios(
+        String name, String surname) {
+        super();
+        this.name = name;
+        this.surname = surname;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+}
